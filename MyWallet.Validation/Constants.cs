d55decfb7175dde3a365134b4832162a3028a122
Mahdi Tahsildari <m.tahsildari@LIVE.CO.UK>
2022-03-31 08:21:45 +0300

    fixed the validation message's bug

--- MyWallet.Validation/Constants.cs
+++ MyWallet.Validation/Constants.cs
@@ -6,13 +6,14 @@ namespace MyWallet.Validation
 {
     public static class Constants
     {
+        public static readonly int LowestAcceptableId = 1;
+        public static readonly double MinimumTransactionAmount = 1;
+
         public static readonly string AccountRequired = "Account is required";
         public static readonly string AmountRequired = "Amount is not valid";
         public static readonly string DirectionInvalid = "Direction is not valid";
         public static readonly string AmountTooLow = $"Minimum transaction amount is {MinimumTransactionAmount}";
                
-        public static readonly int LowestAcceptableId = 1;
-        public static readonly double MinimumTransactionAmount = 0.1D;
     }
 }
 
